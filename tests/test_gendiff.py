d0a80a8e3b1978dc8a0183303b1e9da27e339644
Evgenii <evgen.ak24@gmail.com>
2025-02-07 17:35:24 +0500

    modified

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,13 +3,14 @@ from gendiff.gendiff import generate_diff
 import pytest
 
 
-def get_path(file_name):
-    p = Path(__file__)
-    current_dir = p.absolute().parent
+# Возвращает полный путь к файлу в папке fixtures
+def get_path(file_name: str) -> Path:
+    p: Path = Path(__file__)
+    current_dir: Path = p.absolute().parent
     return current_dir / 'fixtures' / file_name
 
 
-test_data = [
+test_data: list[tuple[str, str, str, str]] = [
     ('file1.json', 'file2.json', 'stylish', 'result_file1_file2'),
     ('file1.yml', 'file2.yml', 'stylish', 'result_file1_file2'),
     ('file3.json', 'file4.json', 'stylish', 'result_file3_file4'),
@@ -21,7 +22,8 @@ test_data = [
 ]
 
 
+# Тестирует функцию generate_diff с разными форматами вывода
 @pytest.mark.parametrize('file1, file2, format_name, result_file', test_data)
-def test_flat_json(file1, file2, format_name, result_file):
-    result = open(get_path(result_file)).read()
+def test_flat_json(file1: str, file2: str, format_name: str, result_file: str) -> None:
+    result: str = open(get_path(result_file)).read()
     assert generate_diff(get_path(file1), get_path(file2), format_name) == result
